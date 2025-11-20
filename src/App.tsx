13f08b904a24f68991b5f76c7841e127424bfba5
Regis <regisjunior2702@gmail.com>
2025-11-20 12:08:07 -0300

    adc a navegação das paginas

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
-function App() {
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Members from './pages/Members'
+import Home from './pages/Home'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const App = () => {
   return (
-    <div></div>
+    <div>
+      <Router>
+        <Header />
+        <Routes>
+          <Route path="/members" element={<Members />} />
+          <Route path="/members/:rm" element={<Members />} />
+          <Route path="/home" element={<Home />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </div>
   )
 }
 
