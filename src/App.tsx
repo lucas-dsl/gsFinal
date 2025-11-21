707cb27efd1686dfd2f8c68dda075679bfa2a8ee
Guilherme <RM561226@fiap.com.br>
2025-11-21 01:31:03 -0300

    ajustando navegação das 3 páginas criadas

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import Members from './pages/Members'
 import Home from './pages/Home'
+import About from './pages/About'
+import Faq from './pages/Faq'
+import Contact from './pages/Contact'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 const App = () => {
@@ -13,6 +16,9 @@ const App = () => {
           <Route path="/members" element={<Members />} />
           <Route path="/members/:rm" element={<Members />} />
           <Route path="/home" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
